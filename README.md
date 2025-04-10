517555948132a0d0738a8595500e6382f27362b6
Itachi546 <silwalsujeet149@gmail.com>
2025-04-10 15:18:52 +0545

    Add Railgun Contract ABIs

---
+++ README.md
@@ -0,0 +1,14 @@
+# `@railgun-reloaded/contract-abis`
+> A Collection of ABI for Interacting with Railgun Contracts
+
+## Install
+```sh
+npm install @railgun-reloaded/contract-abis
+```
+## Example Usage
+```js
+    import {RailgunSmartWalletV1ABI} from '@railgun-reloaded/contract-abis'
+```
+
+# License
+[MIT](LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import RailgunLogicV1ABI from './abis/RailgunLogicV1.json'
+import RailgunSmartWalletV2ABI from './abis/RailgunSmartWalletV2.json'
+import RailgunSmartWalletV21ABI from './abis/RailgunSmartWalletV21.json'
+import RailgunRelayAdaptABI from './abis/RelayAdapt.json'
+
+export { RailgunLogicV1ABI, RailgunSmartWalletV2ABI, RailgunSmartWalletV21ABI, RailgunRelayAdaptABI }
